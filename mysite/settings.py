247423ca091a650092df1a32590520d59fd23b35
Your Name <you@example.com>
2022-07-30 06:54:24 -0500

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+LOGIN_REDIRECT_URL = '/'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
